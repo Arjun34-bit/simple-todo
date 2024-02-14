fa23b38688e697049e32a6a5f05b6916f4d296d5
Arjun Devendra <arjundevendra34@gmail.com>
2024-02-14 15:55:57 +0530

    fix:corrected the cors

--- backend/index.js
+++ backend/index.js
@@ -12,12 +12,7 @@ connectDB();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.get("/", (req, res) => {
   res.send(`
     <html>
